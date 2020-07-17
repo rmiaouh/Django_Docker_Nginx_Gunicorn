4b807cebe565378241123542deecc03312794189
rmiaouh <robinmahou@gmail.com>
2020-07-17 13:02:29 +0200

    nginx debug8

--- leo-ortho/flaskapp/Model.py
+++ leo-ortho/flaskapp/Model.py
@@ -14,9 +14,8 @@ class Model:
     def __init__(self, language):
         self.sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
         dictionary_path = "./opt/{}_50k.txt".format(str("fr"))
-        with open ("./opt/{}_50k.txt".format(str("fr")), 'r') as f :
-	    t = f.readlines()
-            print(t)
+        f = open(dictionary_path, "r")
+        print(f.read())
         self.sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
     def get_spellchecker_from_sentence(self, sentence):
