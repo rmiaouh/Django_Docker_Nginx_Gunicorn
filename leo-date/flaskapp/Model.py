afaec06ca3b757ab2375c736373f435fd8dcc873
rmiaouh <robinmahou@gmail.com>
2020-07-17 13:38:44 +0200

    nginx debug12

--- leo-date/flaskapp/Model.py
+++ leo-date/flaskapp/Model.py
@@ -5,11 +5,8 @@ class Model:
     duckling_model = None
 
     def __init__(self, language):
-        print("model.py ok1")
         self.duckling_model = duckling.DucklingWrapper(language='fr')
-        print("model.py ok2")
+
 
     def get_date_from_sentence(self, sentence):
-        print("model.py ok3")
         return self.duckling_model.parse_time(str(sentence))
-        print("model.py ok4")
--- leo-date/flaskapp/app.py
+++ leo-date/flaskapp/app.py
@@ -3,11 +3,10 @@ from Model import Model
 import os
 
 model_language = 'fr'
-print("app.py ok01")
 model = Model(model_language)
-print("app.py ok02")
+
 app = Flask(__name__)
-print("app.py ok03")
+
 
 @app.route('/health_status', methods=['GET'])
 def get_health_status():
@@ -16,11 +15,10 @@ def get_health_status():
 
 @app.route('/', methods=['POST'])
 def parse_sentence():
-    print("app.py ok1")
+    print("ok")
     sentence = request.get_json().get("sentence")
-    print("app.py ok2")
     output_data = model.get_date_from_sentence(sentence)
-    print("app.py ok3")
+
     return jsonify(data=output_data)
 
 if __name__ == "__main__":
