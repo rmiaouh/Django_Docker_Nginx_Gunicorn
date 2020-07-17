89927929399104296a9a639c90c92e727681ee82
rmiaouh <robinmahou@gmail.com>
2020-07-17 12:45:59 +0200

    nginx debug6

--- leo-ortho/flaskapp/Model.py
+++ leo-ortho/flaskapp/Model.py
@@ -13,7 +13,7 @@ class Model:
 
     def __init__(self, language):
         self.sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-        dictionary_path = "./{}_50k.txt".format(str("fr"))
+        dictionary_path = "./opt/{}_50k.txt".format(str("fr"))
         self.sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
     def get_spellchecker_from_sentence(self, sentence):
